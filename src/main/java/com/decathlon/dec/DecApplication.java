a70f33349398fb9da9e80bf359435984ef9543db
zakaria <zakaria.hachm20@gmail.com>
2023-07-04 16:01:04 +0100

    first commit

---
+++ src/main/java/com/decathlon/dec/DecApplication.java
@@ -0,0 +1,13 @@
+package com.decathlon.dec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/decathlon/dec/DecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decathlon.dec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
