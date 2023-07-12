7b8f97d82be4c11ea959b1ccf3d12a068364bea3
zakaria <zakaria.hachm20@gmail.com>
2023-07-12 21:23:12 +0100

    fix: fixing the spring security

--- src/main/java/com/decathlon/config/SecurityConfiguration.java
+++ src/main/java/com/decathlon/config/SecurityConfiguration.java
@@ -1,1 +1,95 @@
 package com.decathlon.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
+
+import com.decathlon.users.services.MyUserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfiguration {
+    
+    private final MyUserDetailsService userDetailsService;
+
+    public SecurityConfiguration(MyUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    HttpSessionEventPublisher httpSessionEventPublisher() {
+        return new HttpSessionEventPublisher();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+          http.csrf().disable()
+            .authorizeHttpRequests()
+            .requestMatchers(HttpMethod.POST, "/users").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .sessionManagement(session -> session
+                .maximumSessions(1)
+                .sessionRegistry(sessionRegistry())
+                .maxSessionsPreventsLogin(true))
+            .userDetailsService(userDetailsService)
+            .logout(logout -> logout
+            .logoutUrl("/logout")
+            .invalidateHttpSession(true)
+            .deleteCookies("JSESSIONID")
+            .logoutSuccessHandler((request, response, authentication) -> {
+                response.setContentType("application/json");
+                response.getWriter().write("{\"message\": \"You are logged out!\"}");
+            }))
+            .exceptionHandling()
+            .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+
+        JSONLoginConfigurer<HttpSecurity> configurer = new JSONLoginConfigurer<>();
+        configurer.setBuilder(http);
+        http.apply(configurer
+                            .loginPage("/login")
+                            .successHandler((request, response, authentication) -> {
+                                response.setContentType("application/json");
+                                response.getWriter().write("{\"message\": \"Logged in successfully\"}");
+                            })
+                            .failureHandler((request, response, exception) -> {
+                                response.setContentType("application/json");
+                                if (exception instanceof BadCredentialsException) {
+                                    // set status code to 400 (Bad Request) when the username or password is incorrect
+                                    // by default, the response status code is 200 (OK)
+                                    response.setStatus(HttpStatus.BAD_REQUEST.value());
+                                    response.getWriter().write("{\"message\": \"Incorrect username or password\"}");
+                                } else {
+                                    // set status code to 500 (Internal Server Error) for other exceptions
+                                    response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+                                    response.getWriter().write("{\"message\": \"Authentication failed\"}");
+                                }
+                            })
+                            .permitAll());
+
+        return http.build();
+    }
+
+     @Bean
+    SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
+    
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
