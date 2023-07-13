f6ebb03fc44dcc4f35c0b21dac722f609b0dc0d9
zakaria <zakaria.hachm20@gmail.com>
2023-07-13 19:15:21 +0100

    dtos

--- src/main/java/com/decathlon/users/dto/EditPasswordDto.java
+++ src/main/java/com/decathlon/users/dto/EditPasswordDto.java
@@ -1,5 +1,32 @@
 package com.decathlon.users.dto;
 
+import com.decathlon.users.validators.PasswordMatches;
+import com.decathlon.users.validators.VerifyOldPassword;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@PasswordMatches(password = "password", passwordConfirmation = "confirmPassowrd")
 public class EditPasswordDto {
     
+    @NotBlank(message = "The old password is required")
+    @VerifyOldPassword
+    private String oldPassword;
+
+    @NotBlank(message = "The new password is required")
+    @Pattern(regexp = "(?=^.{8,100}$)(?=.*\\d)(?=.*[^A-Za-z0-9]+)(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$", message = "The password must contain at least 1 uppercase, 1 lowercase, 1 special character and 1 digit and must be at least 8 characters")
+    private String password;
+
+    @NotBlank(message = "The password confirmation is required")
+    @JsonProperty("confirmation")
+    private String confirmPassowrd;
 }
--- src/main/java/com/decathlon/users/dto/EditUserDto.java
+++ src/main/java/com/decathlon/users/dto/EditUserDto.java
@@ -1,17 +1,29 @@
 package com.decathlon.users.dto;
-import com.fasterxml.jackson.annotation.JsonProperty;
+
 import com.decathlon.users.enumerations.UserRole;
 import jakarta.annotation.Nullable;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-@Data
-@Builder
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class EditUserDto {
 
-    
+    @Nullable
+    private String firstName;
+
+    @Nullable
+    private String lastName;
+
+    @Nullable
+    @Pattern(regexp = "[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@um5.ac.ma$", message = "The email is invalid")
+    private String email;
+
+    @Nullable
+    private UserRole role;
 }
