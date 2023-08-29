f1d9253815c4d6ce2c5e9621b3d694ae6d89d385
zakaria <zakaria.hachm20@gmail.com>
2023-08-29 19:18:38 +0100

    feat: initiating the comment and absences test

---
+++ src/test/java/com/decathlon/dec/absences/AbsenceControllerTest.java
@@ -0,0 +1,5 @@
+package com.decathlon.dec.absences;
+
+public class AbsenceControllerTest {
+    
+}
---
+++ src/test/java/com/decathlon/dec/commentaires/CommentaireControllerTest.java
@@ -0,0 +1,5 @@
+package com.decathlon.dec.commentaires;
+
+public class CommentaireControllerTest {
+    
+}
